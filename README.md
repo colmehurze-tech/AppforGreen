ca9f899032ebed41dd72c679c6839f6242c89d70
colmehurze <colmehurze@gmail.com>
2025-12-29 21:44:44 +0530

    modified:   README.md 	new file:   main.py 	new file:   test.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AppforGreen
 An electricity tracking app built using python. 
+
+This app tracks the electricity usage on a specified power outlet using python and arduino.
---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import tkinter as tk
+from tkinter import *
+
+app=tk.Tk()
+app.title("AppforGreen")
+app.geometry("400x500")
+label=tk.Label(app,text="Welcome to AppforGreen",font=('FantasqueSansM Nerd Font Propo',20))
+label.pack(padx=20,pady=100)
+app.mainloop()
